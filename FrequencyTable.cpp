2795c935d931b0ab5b097cfd603c3fe473bcbf13
Long Huynh <long.huynh5713@gmail.com>
2023-03-17 08:48:59 +0200

    Implemented frequency counter.

---
+++ FrequencyTable.cpp
@@ -0,0 +1,21 @@
+
+
+#include "FrequencyTable.h"
+
+
+void FrequencyTable::addSymbol(char symbol) {
+    unsigned char location = static_cast<unsigned char>(symbol);
+    FrequencyTable::frequencies[location]++;
+}
+
+int FrequencyTable::getFrequency(char symbol) {
+    unsigned char location = static_cast<unsigned char>(symbol);
+    return FrequencyTable::frequencies[location];
+}
+
+FrequencyTable::FrequencyTable(std::string content) {
+    frequencies.fill(0);
+    for (char symbol: content){
+        this->addSymbol(symbol);
+    }
+}
---
+++ FrequencyTable.h
@@ -0,0 +1,36 @@
+
+#ifndef COMPRESSOR_FREQUENCYTABLE_H
+#define COMPRESSOR_FREQUENCYTABLE_H
+
+
+#include <vector>
+#include <string>
+#include <array>
+
+
+class FrequencyTable {
+private:
+    std::array<int, 256> frequencies;
+
+    /*
+     * Increment the frequency of the symbol.
+     */
+    void addSymbol(char symbol);
+
+public:
+
+    /*
+     * Return the frequency of the symbol.
+     */
+    int getFrequency(char symbol);
+
+
+    /*
+     * Constructor
+     */
+    FrequencyTable(std::string content);
+
+};
+
+
+#endif //COMPRESSOR_FREQUENCYTABLE_H
